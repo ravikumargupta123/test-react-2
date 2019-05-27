91f87746a94e376296b1ea5cfba3c1e4f4b5ba10
ravi gupta <ravi.gupta@happay.in>
2019-05-27 18:57:47 +0530

    Updates

---
+++ precache-manifest.ae5cee3005ed9fe33efe918625cdff48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65fdff24a1e6c2417d7d90d9e3c4415",
+    "url": "/test-react-2/index.html"
+  },
+  {
+    "revision": "65a4d3a801c62222cb81",
+    "url": "/test-react-2/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test-react-2/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "65a4d3a801c62222cb81",
+    "url": "/test-react-2/static/js/main.99a3f623.chunk.js"
+  },
+  {
+    "revision": "dcf9b98f1a592cd33f0b",
+    "url": "/test-react-2/static/js/runtime~main.6fae35a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
